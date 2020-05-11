63d2f0ef9c883cc39f19a50b91d81781ae84e985
Luccas3dvision <luccas.sp7@gmail.com>
2020-05-11 16:01:19 -0300

    Treinamento
    Basico com Javascript

---
+++ ex004.js
@@ -0,0 +1,11 @@
+/* variavel */ 
+var vel = 50 
+
+/* funcao*/ 
+
+/* resultado */
+console.log('A valocidade do seu carro está '+ vel+'km')
+
+if (vel > 60) { // estrutura de descisao simples
+    console.log('Cuidado com a seguraçã')
+    }
